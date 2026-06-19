2a8caacc23d2140a25ad14555b4bbb81840e8180
Fabian Struensee <fabianstruensee@gmail.com>
2026-06-19 20:50:42 +0200

    Get values directly from the calculation

--- 03_1_Analyse_weights.R
+++ 03_1_Analyse_weights.R
@@ -22,10 +22,15 @@ data_wide = data_wide %>%
                           labels = c("NIU", "Employed", "Unemployed"))) %>%
   mutate(STUDENT = factor(
     ifelse(replace(STUDENT, STUDENT == 6, 3) %in% c(1, 2), "Student", "Not student"),
-    levels = c("Not student", "Student")))
+    levels = c("Not student", "Student"))) %>%
+  mutate(
+    SEX = factor(SEX, levels = c(1, 2), 
+                 labels = c("Male", "Female")))
 
 regular_full = data_wide %>%
   svydesign(ids = ~1, weights = ~PERWEIGHT, data = .)
+
+regular_full$variables$EMPSTAT   <- droplevels(regular_full$variables$EMPSTAT)
 
 regular_cleaned = data_cll %>%
   filter(between(AGE, 17, 64))%>%
@@ -35,29 +40,35 @@ regular_cleaned = data_cll %>%
 
 # Calculate mean and SE for the surveys
 svymean(~AGE + factor(SEX) + INCTOT + EXPTOT + EDUCYR + NDUIDMBRS + STUDENT + INSSHR, regular_full)
-svymean(~EMPSTAT, regular_full, na.rm = TRUE)
+svymean(~EMPSTAT, regular_full, na.rm = TRUE) # Remove NA from the EMPSTAT calculation only. The goal is to not influence the mean of the other variables
 
 svymean(~AGE + factor(SEX) + INCTOT + EXPTOT + EDUCYR + NDUIDMBRS + STUDENT + EMPSTAT + INSSHR, regular_cleaned)
 
+
 # Calculate Standard Mean Differences (SMD) for full and cleaned data
-vF <- svyvar(~AGE + INCTOT + EXPTOT + EDUCYR + NDUIDMBRS, regular_full)
-vC <- svyvar(~AGE + INCTOT + EXPTOT + EDUCYR + NDUIDMBRS, regular_cleaned)
-
-vF <- diag(as.matrix(vF))   # pull the variances onto a plain vector
-vC <- diag(as.matrix(vC))
-
 # continuous SMDs: (mean_cleaned - mean_full) / pooled SD
-cont <- c("AGE","INCTOT","EXPTOT","EDUCYR","NDUIDMBRS")
+cont <- c("AGE","INCTOT","EXPTOT","EDUCYR","NDUIDMBRS", "INSSHR")
+fcont <- as.formula(paste0("~", paste(cont, collapse = " + ")))
 
-mF <- c(AGE=40.225, INCTOT=50371, EXPTOT=6106.8, EDUCYR=14.042, NDUIDMBRS=3.2486)
-mC <- c(AGE=40.518, INCTOT=50691, EXPTOT=6631.1, EDUCYR=13.580, NDUIDMBRS=3.2090)
+mF <- coef(svymean(fcont, regular_full))
+mC <- coef(svymean(fcont, regular_cleaned))
+
+vF <- diag(as.matrix(svyvar(fcont, regular_full)))
+vC <- diag(as.matrix(svyvar(fcont, regular_cleaned)))
 
 smd_cont <- (mC[cont] - mF[cont]) / sqrt((vF[cont] + vC[cont]) / 2)
 
 # --- binary SMDs: (p_cleaned - p_full) / pooled SD of a proportion ---
 # read the proportions straight off your svymean output
-pF <- c(male=0.49511, student=0.088823, employed=0.79691)
-pC <- c(male=0.48875, student=0.095229, employed=0.77075)
+pF_all <- coef(svymean(~STUDENT + SEX, regular_full))
+pF <- c(male  = pF_all[["SEXMale"]],
+         student  = pF_all[["STUDENTStudent"]],
+         employed = coef(svymean(~EMPSTAT, regular_full, na.rm = TRUE))[["EMPSTATEmployed"]])
+
+pC_all <- coef(svymean(~EMPSTAT + STUDENT + SEX, regular_cleaned))
+pC <- c(male  = pC_all[["SEXMale"]],
+         student  = pC_all[["STUDENTStudent"]],
+         employed = pC_all[["EMPSTATEmployed"]])
 
 smd_bin <- (pC - pF) / sqrt((pF*(1-pF) + pC*(1-pC)) / 2)
 
