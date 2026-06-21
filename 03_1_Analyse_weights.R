2659af917077f9807df62218a66b4c5fd61f4b69
Fabian Struensee <fabianstruensee@gmail.com>
2026-06-21 12:54:20 +0200

    Edited the mean calculation for EDUCYR, as it contained coded missing vallues (96-99), and created a distribution table

--- 03_1_Analyse_weights.R
+++ 03_1_Analyse_weights.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+library(patchwork)
 library(survey)
 
 # create survey data
@@ -25,7 +27,8 @@ data_wide = data_wide %>%
     levels = c("Not student", "Student"))) %>%
   mutate(
     SEX = factor(SEX, levels = c(1, 2), 
-                 labels = c("Male", "Female")))
+                 labels = c("Male", "Female"))) %>%
+  mutate(EDUCYR = replace(EDUCYR, EDUCYR %in% c(96, 97, 98, 99), NA))
 
 regular_full = data_wide %>%
   svydesign(ids = ~1, weights = ~PERWEIGHT, data = .)
@@ -39,7 +42,8 @@ regular_cleaned = data_cll %>%
   svydesign(ids = ~1, weights = ~PERWEIGHT, data = .)
 
 # Calculate mean and SE for the surveys
-svymean(~AGE + factor(SEX) + INCTOT + EXPTOT + EDUCYR + NDUIDMBRS + STUDENT + INSSHR, regular_full)
+svymean(~AGE + factor(SEX) + INCTOT + EXPTOT + NDUIDMBRS + STUDENT + INSSHR, regular_full)
+svymean(~EDUCYR, regular_full, na.rm = TRUE)
 svymean(~EMPSTAT, regular_full, na.rm = TRUE) # Remove NA from the EMPSTAT calculation only. The goal is to not influence the mean of the other variables
 
 svymean(~AGE + factor(SEX) + INCTOT + EXPTOT + EDUCYR + NDUIDMBRS + STUDENT + EMPSTAT + INSSHR, regular_cleaned)
@@ -48,15 +52,16 @@ svymean(~AGE + factor(SEX) + INCTOT + EXPTOT + EDUCYR + NDUIDMBRS + STUDENT + EM
 # Calculate Standard Mean Differences (SMD) for full and cleaned data
 # continuous SMDs: (mean_cleaned - mean_full) / pooled SD
 cont <- c("AGE","INCTOT","EXPTOT","EDUCYR","NDUIDMBRS", "INSSHR")
-fcont <- as.formula(paste0("~", paste(cont, collapse = " + ")))
 
-mF <- coef(svymean(fcont, regular_full))
-mC <- coef(svymean(fcont, regular_cleaned))
-
-vF <- diag(as.matrix(svyvar(fcont, regular_full)))
-vC <- diag(as.matrix(svyvar(fcont, regular_cleaned)))
-
-smd_cont <- (mC[cont] - mF[cont]) / sqrt((vF[cont] + vC[cont]) / 2)
+smd_one <- function(v) {
+  f  <- as.formula(paste0("~", v))
+  mF <- coef(svymean(f, regular_full,    na.rm = TRUE))
+  mC <- coef(svymean(f, regular_cleaned, na.rm = TRUE))
+  vF <- coef(svyvar(f,  regular_full,    na.rm = TRUE))
+  vC <- coef(svyvar(f,  regular_cleaned, na.rm = TRUE))
+  unname((mC - mF) / sqrt((vF + vC) / 2))
+  }
+smd_cont <- sapply(cont, smd_one)
 
 # --- binary SMDs: (p_cleaned - p_full) / pooled SD of a proportion ---
 # read the proportions straight off your svymean output
@@ -76,4 +81,27 @@ round(abs(c(smd_cont, smd_bin)), 3)
 
 #rm(data_wide, regular_full, regular_cleaned, cols_EMPSTATRD)
 
-# YEAR
+table(regular_full$variables$EDUCYR)
+
+# Density
+plot_df <- rbind(
+  data.frame(EDUCYR = regular_full$variables$EDUCYR,
+             INCTOT = regular_full$variables$INCTOT,
+             EXPTOT = regular_full$variables$EXPTOT,
+             w      = weights(regular_full),
+             source = "Full distribution"),
+  data.frame(EDUCYR = regular_cleaned$variables$EDUCYR,
+             INCTOT = regular_cleaned$variables$INCTOT,
+             EXPTOT = regular_cleaned$variables$EXPTOT,
+             w      = weights(regular_cleaned),
+             source = "Analytical sample")
+)
+
+ggplot(plot_df, aes(x = INCTOT, fill = source, weight = w)) +
+  geom_density(alpha = 0.2, colour = "black", linewidth = 0.3, na.rm = TRUE) +
+  coord_cartesian(xlim = c(0, 250000)) +
+  scale_fill_manual(values = c("Full distribution" = "green",
+                               "Analytical sample" = "red")) +
+  labs(x = "Years of education", y = "Density",
+       fill = NULL, title = "EDUCYR: full vs. analytical sample") +
+  theme_minimal()
